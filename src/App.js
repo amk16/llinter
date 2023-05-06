50362035b279f5060635c174308b90251be5b77c
Ali <alimalik16@hotmail.com>
2023-05-06 14:19:30 +0100

    Initial commit settting up api call to llama server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [inputTexts, setInputTexts] = useState([]);
+  const [generatedTexts, setGeneratedTexts] = useState([]);
+
+  const generateTexts = async () => {
+    const response = await fetch('http://localhost:5000/generate', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ texts: inputTexts })
+    });
+
+    const data = await response.json();
+    setGeneratedTexts(data.generated_texts);
+  };
+
+  const handleInputChange = (index, value) => {
+    const newInputTexts = [...inputTexts];
+    newInputTexts[index] = value;
+    setInputTexts(newInputTexts);
+  };
+
+  const addInputText = () => {
+    setInputTexts([...inputTexts, '']);
+  };
+
+  const removeInputText = index => {
+    const newInputTexts = [...inputTexts];
+    newInputTexts.splice(index, 1);
+    setInputTexts(newInputTexts);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {inputTexts.map((inputText, index) => (
+        <div key={index}>
+          <input type="text" value={inputText} onChange={e => handleInputChange(index, e.target.value)} />
+          <button onClick={() => removeInputText(index)}>Remove</button>
+        </div>
+      ))}
+      <button onClick={addInputText}>Add input</button>
+      <button onClick={generateTexts}>Generate</button>
+      {generatedTexts.map((generatedText, index) => (
+        <p key={index}>{generatedText}</p>
+      ))}
     </div>
   );
 }
