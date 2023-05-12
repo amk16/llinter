8e1536fce3663aea52a162397f32cc9bb5ce2fa9
Ali <alimalik16@hotmail.com>
2023-05-12 02:36:10 +0100

    Correcting api call to server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
   const [inputTexts, setInputTexts] = useState([]);
@@ -12,7 +13,7 @@ function App() {
     });
 
     const data = await response.json();
-    setGeneratedTexts(data.generated_texts);
+    setGeneratedTexts(data.generated_text);
   };
 
   const handleInputChange = (index, value) => {
@@ -32,18 +33,22 @@ function App() {
   };
 
   return (
-    <div>
-      {inputTexts.map((inputText, index) => (
-        <div key={index}>
-          <input type="text" value={inputText} onChange={e => handleInputChange(index, e.target.value)} />
-          <button onClick={() => removeInputText(index)}>Remove</button>
-        </div>
-      ))}
-      <button onClick={addInputText}>Add input</button>
-      <button onClick={generateTexts}>Generate</button>
-      {generatedTexts.map((generatedText, index) => (
-        <p key={index}>{generatedText}</p>
-      ))}
+    <div className="container">
+      <div className="input-col">
+        {inputTexts.map((inputText, index) => (
+          <div key={index}>
+            <input type="text" value={inputText} onChange={e => handleInputChange(index, e.target.value)} />
+            <button onClick={() => removeInputText(index)}>Remove</button>
+          </div>
+        ))}
+        <button onClick={addInputText}>Add input</button>
+        <button onClick={generateTexts}>Generate</button>
+      </div>
+      <div className="output-col">
+        {generatedTexts.map((generatedText, index) => (
+          <p key={index}>{generatedText}</p>
+        ))}
+      </div>
     </div>
   );
 }
